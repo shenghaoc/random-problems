51af56f5dc44ab5f78e95224d7209dc9c518f979
shenghaoc <chen.shenghao@u.nus.edu>
2022-01-19 18:31:51 +0800

    Update count_change.cpp

--- count_change.cpp
+++ count_change.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
-int reduce_change(int amount, int denominations[], int denom)
+int reduce_change(int amount, int *denominations, int denom_len, int denom_index)
 {
+	int denom;
+	if (denom_index < 0)
+	{
+		denom = 0;
+		denom_index = 0;
+	}
+	else
+	{
+		denom = denominations[denom_index];
+	}
 	if (amount == denom)
 	{
 		return 1;
@@ -18,22 +28,25 @@ int reduce_change(int amount, int denominations[], int denom)
 
 	int cnt = 0;
 
-	for (int i = 0; i < sizeof(denominations) / sizeof(denominations[0]); i++)
+	// Start from highest denomination, continue with same and lower denomination
+	// to ensure order
+	// Greedy approach
+	for (int i = denom_len - 1; i >= denom_index; i--)
 	{
-		cnt += reduce_change(amount, denominations, denominations[i]);
+		cnt += reduce_change(amount, denominations, denom_len, i);
 	}
 	return cnt;
 }
 
-int count_change(int amount, int denominations[])
+int count_change(int amount, int *denominations, int denom_len)
 {
 	// How do I avoid duplicates with different order?
-	return reduce_change(amount, denominations, 0);
+	return reduce_change(amount, denominations, denom_len, -1);
 }
 
 int main()
 {
-	int arr[] = {10, 20};
-	cout << count_change(50, arr) << endl;
+	int arr[] = {10, 20, 50};
+	cout << count_change(400, arr, sizeof(arr) / sizeof(arr[0])) << endl;
 	return 0;
 }
